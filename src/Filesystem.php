2256e0f83ce501d10a5cf900e6b81dfd0de0e45f
Nikolas Tsiongas <ntsiongas@gmail.com>
2016-08-29 18:13:39 +0200

    Filesystem : ::put()

---
+++ src/Filesystem.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace RuzovySlon\Filesystem;
+
+use Echo511\TreeTraversal\Tree;
+use League\Flysystem\MountManager;
+
+class Filesystem
+{
+
+	/**
+	 *
+	 * @var MountManager
+	 */
+	protected $flysystem;
+
+	/**
+	 *
+	 * @var Structure
+	 */
+	protected $structure;
+
+	function __construct(MountManager $flysystem, Structure $structure)
+	{
+		$this->flysystem = $flysystem;
+		$this->structure = $structure;
+	}
+
+	public function copy($fromNodePath, $targetNodePath, $newNodeFilesystem = NULL, $newNodeName = NULL)
+	{
+		
+	}
+
+	public function delete($path)
+	{
+		
+	}
+
+	public function has($path)
+	{
+		
+	}
+
+	public function query(IQueryObject $queryObject)
+	{
+		
+	}
+
+	public function read($path)
+	{
+		
+	}
+
+	public function rename($path, $name)
+	{
+		
+	}
+
+	public function url($path)
+	{
+		
+	}
+
+	public function put($pathWithStorage, $contents)
+	{
+		list($storage, $path) = explode('://', $pathWithStorage, 2);
+		$path = PathHelper::sanitize($path);
+		$status = $this->flysystem->put($pathWithStorage, $contents);
+
+		if ($status) {
+			$this->structure->insertNode($storage, $path);
+			return $this->structure->hasNode($path);
+		}
+
+		return FALSE;
+	}
+
+}
---
+++ tests/unit/FilesystemCest.php
@@ -0,0 +1,55 @@
+<?php
+
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem as Filesystem2;
+use League\Flysystem\MountManager;
+use RuzovySlon\Filesystem\Filesystem;
+
+class FilesystemCest
+{
+
+	/**
+	 *
+	 * @var MountManager
+	 */
+	private $flysystem;
+
+	public function _before(UnitTester $I)
+	{
+		$I->deleteDir($this->getFilesystemFilePath(''));
+		$memoryAdapter = new Local($this->getFilesystemFilePath(''));
+		$memoryFilesystem = new Filesystem2($memoryAdapter);
+		$filesystems = [
+			'local' => $memoryFilesystem,
+		];
+		$this->flysystem = new MountManager($filesystems);
+	}
+
+	public function _after(UnitTester $I)
+	{
+		$I->deleteDir($this->getFilesystemFilePath(''));
+	}
+
+	public function testPut(UnitTester $I)
+	{
+		$structure = Mockery::mock('\RuzovySlon\Filesystem\Structure')
+			->shouldReceive('insertNode')
+			->once()
+			->with('local', '/root/1st/file.txt')
+			->andReturn(TRUE)
+			->shouldReceive('hasNode')
+			->once()
+			->with('/root/1st/file.txt')
+			->andReturn(TRUE)
+			->getMock();
+		$filesystem = new Filesystem($this->flysystem, $structure);
+		$filesystem->put('local://root/1st/file.txt', 'ANDROMEDA');
+		$I->seeFileFound($this->getFilesystemFilePath('/root/1st/file.txt'));
+	}
+
+	protected function getFilesystemFilePath($path)
+	{
+		return __DIR__ . '/../_output/filesystem' . $path;
+	}
+
+}
